20e5f1dbc4af14e4c9da232cb592863310c128b3
pete.davison@gmail.com <pete.davisson@gmail.com>
2020-12-11 15:47:26 -0500

    Created dashboard templates

---
+++ Controllers/CRMController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CRM.Models;
+
+namespace CRM.Controllers
+{
+    public class CRMController : Controller
+    {
+        private readonly ILogger<CRMController> _logger;
+
+		public CRMController(ILogger<CRMController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+    }
+}
