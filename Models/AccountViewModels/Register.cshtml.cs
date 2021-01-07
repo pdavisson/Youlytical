12f8af51995f5fc41b8cde92dd5381c0d615e51c
pete.davison@gmail.com <pete.davisson@gmail.com>
2021-01-07 11:37:42 -0500

    Update to Registration Page

--- Models/AccountViewModels/Register.cshtml.cs
+++ Models/AccountViewModels/Register.cshtml.cs
@@ -8,10 +8,33 @@ namespace CRM.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
+        [DataType(DataType.Text)]
+        [Display(Name="Company")]
+        public string Company {get; set; }
+
+        [DataType(DataType.Text)]
+        [Display(Name="FranchiseID")]
+        public string FranchiseID {get; set; }
+        
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name="FirstName")]
+        public string FirstName {get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name="LastName")]
+        public string LastName {get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
