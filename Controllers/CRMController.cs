2c2a9d286f94936d59c4027079bd4e279630875f
pete.davison@gmail.com <pete.davisson@gmail.com>
2021-01-08 14:50:55 -0500

    Nothing really

--- Controllers/CRMController.cs
+++ Controllers/CRMController.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using CRM.Models;
 using Microsoft.AspNetCore.Authorization;
 
 namespace CRM.Controllers
