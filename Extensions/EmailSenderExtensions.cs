f50bb2566c5ce5ac2e2f3a60dd9c93c96c90deff
pete.davison@gmail.com <pete.davisson@gmail.com>
2021-02-24 19:06:48 -0500

    Integrated SendGrid Dynamic Template

--- Extensions/EmailSenderExtensions.cs
+++ Extensions/EmailSenderExtensions.cs
@@ -11,8 +11,7 @@ namespace CRM.Services
     {
         public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
         {
-            return emailSender.SendEmailAsync(email, "Confirm your email",
-                $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+            return emailSender.SendEmailAsync(email, "Confirm your email", link);
         }
     }
 }
--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 using SendGrid;
 using SendGrid.Helpers.Mail;
 
@@ -36,7 +37,11 @@ namespace CRM.Services
                 HtmlContent = message
             };
             msg.AddTo(new EmailAddress(email));
-
+            msg.SetTemplateId("d-d5189e1fbbc1459aadeb9049cc0844fe");
+            msg.SetTemplateData(new ButtonURL
+            {
+                ConfirmURL=message
+            });
             // Disable click tracking.
             // See https://sendgrid.com/docs/User_Guide/Settings/tracking.html
             msg.SetClickTracking(false, false);
@@ -44,6 +49,11 @@ namespace CRM.Services
             return client.SendEmailAsync(msg);
             
         }
+            private class ButtonURL
+            {
+                [JsonProperty("ConfirmURL")]
+                public string ConfirmURL { get; set; }
+            }
     
     
     //ORIGINAL FROM HERE DOWN, EVERYTHING ABOVE THIS AND BELOW PUBLIC CLASS EMAILSENDER : IEMAILSENDER CAN BE DELETED TO REVERT BACK TO START
