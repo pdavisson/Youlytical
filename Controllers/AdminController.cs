be929576b884b117605653f75fe6326d267e1be1
pete.davison@gmail.com <pete.davisson@gmail.com>
2021-02-28 15:13:59 -0500

    Continued Improvement User Manager

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,15 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CRM.Models;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using CRM.Services;
+using CRM.Data;
+using System.Linq;
 
 namespace CRM.Controllers 
 {
@@ -19,11 +16,39 @@ namespace CRM.Controllers
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly IEmailSender _emailSender;
         private readonly ILogger _logger;
-
-        [Authorize(Roles ="Admin")]
+        private readonly ApplicationDbContext _context;
+        public AdminController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            IEmailSender emailSender,
+            ILogger<AdminController> logger,
+            ApplicationDbContext context)            
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
+            _logger = logger;
+            _context = context;
+        }
+        [Authorize(Roles ="Manager, Admin")]
         public IActionResult ManageUsers()
 		{
-			return View();
+            var UserID=_userManager.GetUserId(HttpContext.User);
+            ApplicationUser user = _userManager.FindByIdAsync(UserID).Result;
+            if(HttpContext.User.IsInRole("Admin"))
+            {
+                _logger.LogInformation("User logged in is Admin.");
+                var list = _context.Users.Where(a=>a.Company==user.Company).ToList();
+                ViewBag.ContactData=list;
+            }
+            else
+            {
+                _logger.LogInformation("User logged in is Manager.");
+                var list = _context.Users.Where(a=>a.Company==user.Company && a.FranchiseID==user.FranchiseID).ToList();
+                ViewBag.ContactData=list;
+            }
+
+            return View();
 		}
     }
 }
